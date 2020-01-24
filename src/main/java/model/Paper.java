ed85b7ce778de0fa6128bce951b9711d458c0ac2
Jon Bos <bos.jon@gmail.com>
2020-01-24 08:02:40 -0600

    Initial commit

---
+++ src/main/java/model/Paper.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Paper {
+
+	public Paper() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/test/TestPencil.java
@@ -0,0 +1,21 @@
+package test;
+import model.Pencil;
+import model.Paper;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestPencil {
+	Pencil pencil = new Pencil();
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
